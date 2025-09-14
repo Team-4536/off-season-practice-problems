765903cc749c78c8e4155210c16980b7e6aae378
connormaass <connormaass2@gmail.com>
2025-09-14 18:19:37 -0500

    Formatting is fun

--- src/robot.py
+++ src/robot.py
@@ -1,19 +1,21 @@
 import wpilib
-from timing import TimeData  
 import rev
 from rev import SparkMax
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
+
         self.driveCtrlr = wpilib.XboxController(0)
 
         self.Motor =  SparkMax(2, rev.SparkMax.MotorType.kBrushless)
+
         self.Motor.set(0)
         
     def robotPeriodic(self) -> None:
        pass
         
     def teleopInit(self) -> None:
+        
         self.on = False
 
     def teleopPeriodic(self):
@@ -22,8 +24,10 @@ class Robot(wpilib.TimedRobot):
         
         if pressedA:
             self.on = not self.on
+
         if self.on:
             self.Motor.set(0.2)
+
         else:
             self.Motor.set(0)
        
