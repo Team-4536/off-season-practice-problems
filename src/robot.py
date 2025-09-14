5fbd0a9b67ebacd6d99759c77adb150e17d4a698
connormaass <connormaass2@gmail.com>
2025-09-14 18:16:39 -0500

    made simple

--- src/robot.py
+++ src/robot.py
@@ -5,9 +5,9 @@ from rev import SparkMax
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-        self.driveCtrlr = wpilib.XboxController(0)
 
         self.Motor =  SparkMax(2, rev.SparkMax.MotorType.kBrushless)
+        
         self.Motor.set(0)
         
     def robotPeriodic(self) -> None:
