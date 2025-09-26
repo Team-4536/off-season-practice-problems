7ce49d422f3a361757cc3125b849ccb017a7c073
davidorser <davidorser@gmail.com>
2025-09-26 08:40:32 -0500

    Teaching Template Auto programming and Autostage

---
+++ djoAuto.py
@@ -0,0 +1,52 @@
+import robot
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from robot import Robot
+
+
+class AutoStage:
+    def __init__(self):
+        # This is the constructor (startup code), declare your variables here.
+        # It will be called when the robot starts.
+        pass
+
+    def run(self, r: "Robot"):
+        # this will be called repeatedly every 20ms (e.g., loop())
+        pass
+
+    def isDone(self, r: "Robot") -> bool:
+        # this will be called at the end of the run() and if it returns true will stop the stage
+        return True
+
+    def autoInit(self, r: "Robot"):
+        # This is similar to __init__ but it will be called when Auto Begins, it is useful for
+        # resetting encode positions or reseting gyros, etc.  Anything that should be reset each
+        # auto run. (e.g., when running Auto on the practice field.)
+        pass
+
+
+class myCustomAS(AutoStage):
+    def __init__(self, numSec: float = 20):
+        self.myCounter = 5 * numSec  # 20ms per cycle, 50 cycles = 1 second
+        pass
+
+    def run(self, r: "Robot"):
+        self.myCounter = self.myCounter - 1
+        r.Motor.set(0.2)
+
+        pass
+
+    def isDone(self, r: "Robot") -> bool:
+        if self.myCounter <= 0:
+            r.Motor.set(0.0)
+            return False
+        return True
+
+    def autoInit(self, r: "Robot"):
+        pass
+
+
+## TODO: add an example similar to above, but remove counter and use Timer Class.
+## TODO: Decide on a datastructure (dictionary?) and implement a (branching?) queue system.
+#          If its branching...makes sure to do a simple non-branching example first.
--- src/robot.py
+++ src/robot.py
@@ -1,27 +1,40 @@
 import wpilib
 import rev
+import djoAuto
 from rev import SparkMax
+
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
 
         self.driveCtrlr = wpilib.XboxController(0)
 
-        self.Motor =  SparkMax(2, rev.SparkMax.MotorType.kBrushless)
+        self.Motor = SparkMax(2, rev.SparkMax.MotorType.kBrushless)
 
         self.Motor.set(0)
-        
+        self.myOneAutoStage = djoAuto.myCustomAS(5)
+        self.myTwoAutoStage = djoAuto.myCustomAS(10)
+
     def robotPeriodic(self) -> None:
-       pass
-        
+        pass
+
+    def autonomousInit(self) -> None:
+        self.myOneAutoStage.autoInit(self)
+        pass
+
+    def autonomousPeriodic(self):
+        if not self.myOneAutoStage.isDone(self):
+            self.myOneAutoStage.run(self)
+        pass
+
     def teleopInit(self) -> None:
-        
+
         self.on = False
 
     def teleopPeriodic(self):
-        
+
         pressedA = self.driveCtrlr.getAButtonPressed()
-        
+
         if pressedA:
             self.on = not self.on
 
@@ -30,7 +43,7 @@ class Robot(wpilib.TimedRobot):
 
         else:
             self.Motor.set(0)
-       
+
 
 if __name__ == "__main__":
     wpilib.run(Robot)
