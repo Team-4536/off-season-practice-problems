e471d2aca634fb90e4657d671af108c4df86dbd9
davidorser <davidorser@gmail.com>
2025-10-09 22:23:22 -0500

    Still messy, but semi-functional 3 stage auto example

--- src/djoAuto.py
+++ src/djoAuto.py
@@ -1,5 +1,8 @@
 import robot
 from typing import TYPE_CHECKING, Callable
+from ntcore import NetworkTableInstance, NetworkTable
+
+# from __future__ import annotations
 
 if TYPE_CHECKING:
     from robot import Robot
@@ -7,8 +10,24 @@ if TYPE_CHECKING:
 
 class AutoStage:
     def __init__(self):
+        self.name = (
+            "give every AutoStage a unique name with __init__ args"  # + str(numSec)
+        )
+        # all auto stages should report their name and optionally other debug info
+        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
+
         # This is the constructor (startup code), declare your variables here.
         # It will be called when the robot starts.
+        pass
+
+    def setNext(self, nextAS: "AutoStage"):
+        self.nextStage = nextAS
+        pass
+
+    def autoInit(self, r: "Robot"):
+        # This is similar to __init__ but it will be called immediately before the Auto Stage
+        # begins, it is useful for resetting encode positions or reseting gyros, etc.
+        #  Anything that should be reset each auto run.
         pass
 
     def run(self, r: "Robot"):
@@ -19,40 +38,86 @@ class AutoStage:
         # this will be called at the end of the run() and if it returns true will stop the stage
         return True
 
-    def autoInit(self, r: "Robot"):
-        # This is similar to __init__ but it will be called when Auto Begins, it is useful for
-        # resetting encode positions or reseting gyros, etc.  Anything that should be reset each
-        # auto run. (e.g., when running Auto on the practice field.)
-        pass
-
     def getNext(self, r: "Robot"):
         # This Function contains a string that indicates which stage runs next. It must match
         # those strings created in robot.py (TOOD: Find a better way to link strings to AS objs.)
-        return "stage2"
+        self.nextStage
         pass
 
 
 class myCustomAS(AutoStage):
     def __init__(self, numSec: float = 20):
-        self.myCounter = 5 * numSec  # 20ms per cycle, 50 cycles = 1 second
+        self.name = "myCustomAS:" + str(numSec)
+        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
+
+        self.myCounterStart: float = 5 * numSec  # 20ms per cycle, 50 cycles = 1 second
+        self.myCounter = self.myCounterStart
+
         pass
 
     def run(self, r: "Robot"):
         self.myCounter = self.myCounter - 1
         r.Motor.set(0.2)
+        self.table.putString("AutoStage", self.name)
+        self.table.putNumber("Counter Remaining", self.myCounter)
 
         pass
 
     def isDone(self, r: "Robot") -> bool:
+        self.table.putNumber("test", self.myCounter)
         if self.myCounter <= 0:
             r.Motor.set(0.0)
-            return False
-        return True
+            return True  # are these backward?
+        return False
+
+    def autoInit(self, r: "Robot"):
+        self.myCounter = self.myCounterStart  # 20ms per cycle, 50 cycles = 1 second
+        pass
+
+    def setNext(self, nextAS: AutoStage):
+        self.nextStage = nextAS
+        pass
+
+    # Example setNext() function for branching auto
+    # def setNext(self, nextAS1: AutoStage, nextAS2: AutoStage):
+    #     self.nextStage1 = nextAS1
+    #     self.nextStage2 = nextAS2
+    #     pass
+
+    def getNext(self, r: "Robot"):
+        # Example Branching Auto Code:
+        # if True:
+        #     self.nextStage = self.nextStage1
+        # else:
+        #     self.nextStage = self.nextStage2
+        return self.nextStage
+        pass
+
+
+class allStopAS(AutoStage):
+    # Every Robot must have a Auto Stage that safely stops all activity!
+    def __init__(self):
+        self.name = "allStopAS:"
+        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
+        pass
+
+    def run(self, r: "Robot"):
+        self.table.putString("AutoStage", self.name)
+        r.Motor.set(0.0)
+        pass
+
+    def isDone(self, r: "Robot") -> bool:
+        return False
 
     def autoInit(self, r: "Robot"):
         pass
 
+    def setNext(self):
+        pass
 
-## TODO: add an example similar to above, but remove counter and use Timer Class.
-## TODO: Decide on a datastructure (dictionary?) and implement a (branching?) queue system.
-#          If its branching...makes sure to do a simple non-branching example first.
+    def getNext(self, r: "Robot"):
+        return self
+        pass
+
+
+## TODO: Replace counter with a Timer Object
--- src/robot.py
+++ src/robot.py
@@ -2,6 +2,7 @@ import wpilib
 import rev
 import djoAuto
 from rev import SparkMax
+from ntcore import NetworkTableInstance, NetworkTable
 
 
 class Robot(wpilib.TimedRobot):
@@ -10,33 +11,37 @@ class Robot(wpilib.TimedRobot):
         self.driveCtrlr = wpilib.XboxController(0)
 
         self.Motor = SparkMax(2, rev.SparkMax.MotorType.kBrushless)
-
         self.Motor.set(0)
+
         self.myOneAutoStage = djoAuto.myCustomAS(5)
         self.myTwoAutoStage = djoAuto.myCustomAS(10)
 
-        self.autoDict = {
-            "stage1": djoAuto.myCustomAS(5),
-            "stage2": djoAuto.myCustomAS(10),
-            "stage3": self.myOneAutoStage,
-        }
-        self.curAutoStage = self.autoDict["stage1"]
-
-        # self.autoDictionary["stage3"] = djoAuto.myCustomAS(2)
+        auto1_start = djoAuto.myCustomAS(10)
+        auto1_shoot = djoAuto.myCustomAS(5)
+        auto1_moveBack = djoAuto.myCustomAS(2)
+        auto1_allStop = djoAuto.allStopAS()
+        auto1_start.setNext(auto1_shoot)
+        auto1_shoot.setNext(auto1_moveBack)
+        auto1_moveBack.setNext(auto1_allStop)
+        self.curAutoStage = auto1_start
+        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
 
     def robotPeriodic(self) -> None:
         pass
 
     def autonomousInit(self) -> None:
         self.curAutoStage.autoInit(self)
+        self.table.putNumber("test", 2)
         pass
 
     def autonomousPeriodic(self):
         if self.curAutoStage.isDone(self) == False:
             self.curAutoStage.run(self)
         else:
-            nextStageID = self.curAutoStage.getNext(self)
-            curAutoStage = self.autoDict[nextStageID]
+            self.curAutoStage = self.curAutoStage.getNext(self)
+            self.table.putString("NextStage", self.curAutoStage.getNext(self).name)
+
+            self.curAutoStage.autoInit(self)
         pass
 
     def teleopInit(self) -> None:
@@ -56,6 +61,30 @@ class Robot(wpilib.TimedRobot):
         else:
             self.Motor.set(0)
 
+    def DisabledPeriodic(self):
+        pass
+
+    def SimulationPeriodic(self):
+        pass
+
+    def TestPeriodic(self):
+        pass
+
 
 if __name__ == "__main__":
     wpilib.run(Robot)
+
+    # self.autoDict = {
+    #     "stage2": djoAuto.myCustomAS(10, "stage3"),
+    #     "stage1": djoAuto.myCustomAS(5, "stage2"),
+    #     "stage3": self.myOneAutoStage,
+    #     # "stageN": djoAuto.MoveToShoot(
+    #     #     "../paths/mypath.json", "stageIDforInPos", "stageIDforOutPos"
+    #     # ),
+    # }
+
+    # self.autos = {"three piece", forward}
+    # self.ThreePiece = forward
+    # self.curAutoStage = self.autoDict["stage1"]
+
+    # # self.autoDictionary["stage3"] = djoAuto.myCustomAS(2)
