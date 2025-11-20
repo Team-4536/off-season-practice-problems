0e61f94621195c212ce8509f43f363840ce76d0e
davidorser <davidorser@gmail.com>
2025-11-20 16:28:46 -0600

    minimally non-working example

--- src/robot.py
+++ src/robot.py
@@ -4,9 +4,83 @@ import djoAuto
 from rev import SparkMax
 from ntcore import NetworkTableInstance, NetworkTable
 
+from pathplannerlib.path import PathPlannerPath, PathPlannerTrajectory
+from pathplannerlib.config import RobotConfig, ModuleConfig, DCMotor
+from wpimath import units
+from wpimath.geometry import Translation2d, Pose2d, Rotation2d
+from wpimath.kinematics import ChassisSpeeds
+
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
+        oneftInMeters = units.feetToMeters(1)
+        mass = units.lbsToKilograms(122)
+        moi = (  # moi = 1/6 * m * side_in_meters^2 , for a cube 32x32inches :
+            (1 / 6.0)
+            * mass
+            * (32.0 / 12.0 * oneftInMeters)
+            * (32.0 / 12.0 * oneftInMeters)
+        )
+        motor = DCMotor(12, 2.6, 105, 1.8, 594.39, 1)  # old RPM = 5676
+        modConfig = ModuleConfig(0.05, 1.1, 1.5, motor, 42, 1)  # old COF = 9.5
+        m = DCMotor(12, 2.6, 105, 1.8, 594.39, 1)  # old RPM = 5676
+
+        traj = PathPlannerTrajectory(
+            path=PathPlannerPath.fromPathFile("test"),
+            starting_speeds=ChassisSpeeds(0, 0),
+            starting_rotation=Rotation2d(0),
+            config=RobotConfig(
+                massKG=mass,
+                MOI=moi,
+                moduleConfig=ModuleConfig(
+                    wheelRadiusMeters=0.05,
+                    maxDriveVelocityMPS=1.1,
+                    wheelCOF=1.5,
+                    driveMotor=motor,
+                    driveCurrentLimit=42,
+                    numMotors=1,
+                ),
+                moduleOffsets=[
+                    Translation2d(-oneftInMeters, oneftInMeters),
+                    Translation2d(oneftInMeters, oneftInMeters),
+                    Translation2d(-oneftInMeters, -oneftInMeters),
+                    Translation2d(oneftInMeters, -oneftInMeters),
+                ],
+            ),
+        )
+        print(traj.getTotalTimeSeconds())
+        breakpoint()
+
+        RConfig = RobotConfig(
+            mass,
+            moi,
+            modConfig,
+            [
+                Translation2d(-oneftInMeters, oneftInMeters),
+                Translation2d(oneftInMeters, oneftInMeters),
+                Translation2d(-oneftInMeters, -oneftInMeters),
+                Translation2d(oneftInMeters, -oneftInMeters),
+            ],
+        )
+        p = PathPlannerPath.fromPathFile("test")
+        myTraj = p.generateTrajectory(
+            starting_speeds=ChassisSpeeds(0, 0),  # starting speeds = 0 in x,y,rot
+            starting_rotation=Rotation2d(0),
+            # p.getStartingHolonomicPose().rotation(),
+            config=RConfig,
+        )
+        print(myTraj.getTotalTimeSeconds())
+        breakpoint()
+
+        nextTrajState = myTraj.sample(1.3)
+        print(nextTrajState.timeSeconds)
+        print(myTraj.getState(4).timeSeconds)
+
+        initialPose = Pose2d(
+            myTraj.getInitialPose().translation(),
+            nextTrajState.holonomicRotation,
+        )
+        breakpoint()
 
         self.driveCtrlr = wpilib.XboxController(0)
 
