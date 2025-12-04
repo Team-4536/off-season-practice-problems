cfa7999131c66e5ad1878cbcb4bc9ccf5f94cb22
davidorser <davidorser@gmail.com>
2025-12-04 16:51:50 -0600

    clean up robot.py again

--- src/robot.py
+++ src/robot.py
@@ -1,23 +1,19 @@
 import wpilib
-import rev
-from rev import SparkMax
+
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
+        pass
 
-        self.Motor =  SparkMax(2, rev.SparkMax.MotorType.kBrushless)
-        
-        self.Motor.set(0)
-        
     def robotPeriodic(self) -> None:
-       pass
-        
+        pass
+
     def teleopInit(self) -> None:
-        self.Motor.set(0.2)
+        pass
 
     def teleopPeriodic(self):
         pass
-       
+
 
 if __name__ == "__main__":
     wpilib.run(Robot)
