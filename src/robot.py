7a1c6e1f1feed83159409b005b5b1ef6de3c1101
davidorser <davidorser@gmail.com>
2025-12-04 17:13:53 -0600

    Fixed robot.py erasure

--- src/robot.py
+++ src/robot.py
@@ -1,25 +1,21 @@
 import wpilib
-import rev
-from rev import SparkMax
+from ntcore import NetworkTableInstance
 
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-        self.Motor = SparkMax(2, SparkMax.MotorType.kBrushless)
-
-        self.motorCtrlr = wpilib.XboxController(0)
-
-        self.Motor.set(0)
+        self.table = NetworkTableInstance.getDefault().getTable("telementry")
+        self.table.putNumber("voltage", 1)
 
     def robotPeriodic(self) -> None:
         pass
 
     def teleopInit(self) -> None:
-        pass
+        voltage = 5
+        self.table.putNumber("voltage", voltage)
 
     def teleopPeriodic(self):
-
-        aDown = self.motorCtrlr.getAButton()
+        pass
 
 
 if __name__ == "__main__":
