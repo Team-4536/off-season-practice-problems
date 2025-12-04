71e0750bca170ca9fecfd12bdb8cdc05784eed3f
davidorser <davidorser@gmail.com>
2025-12-04 17:22:44 -0600

    Added Step-2 content back to this step.

--- src/robot.py
+++ src/robot.py
@@ -1,21 +1,41 @@
 import wpilib
 from ntcore import NetworkTableInstance
+import rev
+from rev import SparkMax
 
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
+
+        self.driveCtrlr = wpilib.XboxController(0)
+
+        self.Motor = SparkMax(2, rev.SparkMax.MotorType.kBrushless)
+
+        self.Motor.set(0)
         self.table = NetworkTableInstance.getDefault().getTable("telementry")
         self.table.putNumber("voltage", 1)
 
     def robotPeriodic(self) -> None:
-        pass
+        self.table.putNumber(
+            "motorOutput", self.Motor.getAppliedOutput()  # This doesn't work! Yet...
+        )
 
     def teleopInit(self) -> None:
+        self.on = False
+
         voltage = 5
         self.table.putNumber("voltage", voltage)
 
     def teleopPeriodic(self):
-        pass
+        pressedA = self.driveCtrlr.getAButtonPressed()
+
+        if pressedA:
+            self.on = not self.on
+
+        if self.on:
+            self.Motor.set(0.2)
+        else:
+            self.Motor.set(0)
 
 
 if __name__ == "__main__":
